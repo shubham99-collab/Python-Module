45995a3c4dfc4b4715bfecc3ce1c55e72f442274
shubham99-collab <shubham15975399@gmail.com>
2025-08-29 13:14:59 +0530

    Add files via upload

---
+++ Built In Module.py
@@ -0,0 +1,30 @@
+#>> Built In Module
+
+# >> Date time Module
+# import datetime
+# x=datetime.datetime.now()
+# print(x)
+
+# >>> Calender Module
+# import calendar
+# cal = calendar.month(2025,8)
+# print ("calendar:")
+# print (cal)
+
+# >> Maths Module
+# import math  
+# x = math.sqrt(25)
+# print(x)
+
+#>>> Maths Module
+# import math
+# x = math.pi
+# print(x)
+
+# import platform
+# x = platform.system()
+# print(x)
+
+# import platform
+# x = dir(platform)
+# print(x)
---
+++ User defined module.py
@@ -0,0 +1,30 @@
+# import arithmetic
+# print(arithmetic.add(5, 9))
+# print(arithmetic.sub(7, 6))
+# print(arithmetic.mul(4, 8))
+# print(arithmetic.div(8, 4))
+
+# from arithmetic import add
+# print(add(4, 9))
+
+# from arithmetic import *
+# print(add(8, 6))
+# print(sub(6, 2))
+# print(mul(5, 6))
+# print(div(8, 2))
+
+# import arithmetic
+# print(arithmetic.add(5, 7))
+# del arithmetic
+# print(arithmetic.mul(5, 7))
+
+#import arithmetic
+# print(dir(arithmetic))
+
+# import arithmetic
+# print(arithmetic._file_)
+# print(arithmetic._name_) 
+
+import mymodule
+a=mymodule.cube(2)
+print(a)
